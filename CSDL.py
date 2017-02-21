41559d3eecb67fd2abcea79d55ae9e19baa18c63
michael teti <mtetifau@gmail.com>
2017-02-21 08:07:40 -0500

    alternating random and trainable convolutional layers

--- CSDL.py
+++ CSDL.py
@@ -24,10 +24,10 @@ from scipy.misc import imshow
 from tflearn.data_augmentation import ImageAugmentation
 
 import tflearn.datasets.oxflower17 as oxflower17
-X, Y = oxflower17.load_data(one_hot=True, resize_pics=(150, 150))
+X, Y = oxflower17.load_data(one_hot=True, resize_pics=(170, 170))
 X=X.reshape([X.shape[0], -1])
 X=(X-np.mean(X, axis=0))/(np.std(X, axis=0)+1e-6)
-X=X.reshape([X.shape[0], 150, 150, 3])
+X=X.reshape([X.shape[0], 170, 170, 3])
 img_aug = ImageAugmentation()
 img_aug.add_random_rotation(max_angle=33.)
 #X=np.mean(X, axis=3)
@@ -39,7 +39,7 @@ img_aug.add_random_rotation(max_angle=33.)
 #X=X.reshape([X.shape[0], 10, 10, 1])
 
 # Building 'AlexNet'
-network = input_data(shape=[None, 150, 150, 3], data_augmentation=img_aug)
+network = input_data(shape=[None, 170, 170, 3], data_augmentation=img_aug)
 network = conv_2d(network, 96, 11, strides=4, weights_init='normal', trainable=False)
 network = max_pool_2d(network, 3, strides=2)
 network = local_response_normalization(network)
@@ -48,11 +48,11 @@ network = max_pool_2d(network, 3, strides=2)
 network = local_response_normalization(network)
 network = conv_2d(network, 384, 3, weights_init='normal', trainable=False)
 network = conv_2d(network, 384, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu')
+network = conv_2d(network, 256, 3, weights_init='normal', trainable=False)
 network = max_pool_2d(network, 3, strides=2)
 network = local_response_normalization(network)
-network = fully_connected(network, 4096, activation='tanh')
-network = dropout(network, 0.5)
+#network = fully_connected(network, 4096, activation='tanh')
+#network = dropout(network, 0.5)
 network = fully_connected(network, 4096, activation='tanh')
 network = dropout(network, 0.5)
 network = fully_connected(network, 17, activation='softmax')
@@ -65,4 +65,4 @@ model = tflearn.DNN(network, checkpoint_path='model_alexnet',
                     max_checkpoints=1, tensorboard_verbose=3)
 model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True,
           show_metric=True, batch_size=64, snapshot_step=200,
-snapshot_epoch=False, run_id='cifar10_altconvrand2_size170')
+snapshot_epoch=False, run_id='oxfordflowers_alternatingrandconvlayers_onefc')
