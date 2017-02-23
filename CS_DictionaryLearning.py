f3244348304b29cea539b298fdf52979bb5123ee
michael teti <mtetifau@gmail.com>
2017-02-23 14:32:46 -0500

    needs some work

--- CS_DictionaryLearning.py
+++ CS_DictionaryLearning.py
@@ -60,6 +60,46 @@ def visualize_dict(D, d_shape, patch_shape):
   imshow(vis_d)
 
 
+
+class ELM(object):
+  '''Single, fully-connected hidden layer extreme learning machine.
+     
+     Args: 
+          input_data: training examples with shape n x num_features.
+          input_labels: n-dimensional one-hot vector. '''
+  
+  def __init__(self, training_data, training_labels, testing_data, testing_labels, batch_sz):
+    self.X=training_data
+    self.Y=training_labels
+    self.x_=testing_data
+    self.y_=testing_labels
+    self.batch_sz=batch_sz
+    self.prediction
+    self.optimize
+    self.accuracy
+
+  def prediction(self):
+    batch=self.X[np.uint8(np.floor(np.random.rand(self.batch_sz)*self.X.shape[0])), :]
+    net=tf.matmul(batch, tf.random_normal([36*18, 36*18/2], dtype=tf.float32))
+    net=tf.nn.relu(net+tf.Variable(tf.constant(0.1)))
+    net=tf.matmul(net, tf.Variable(tf.truncated_normal([36*18/2, 1], dtype=tf.float32)))
+    net=tf.nn.sigmoid(net+tf.Variable(tf.constant(0.1, shape=[1, ])))
+    return sess.run(net)
+
+  def optimize(self):
+    cost=tf.reduce_sum((self.Y-self.prediction())**2, reduction_indices=0)
+    train_step=tf.train.AdamOptimizer(1e-4).minimize(cost)
+    return sess.run(train_step)   
+ 
+  def accuracy(self):
+    accuracy=tf.reduce_mean(tf.cast(tf.equal(tf.round(self.prediction()), self.Y), tf.float32))
+    return sess.run(accuracy)
+
+  def validate(self):
+    validation_acc=
+
+
+
 ps=[36, 18]  # size of the images
 measurements=300
 k=400 # number of patches in dictionary
@@ -70,14 +110,14 @@ absent=data['C1']
 present=data['C2']
 
 # reshape into vectors and divide for training and testing
-absent_train=absent[:900, :, :].reshape([900, -1])
-present_train=present[:900, :, :].reshape([900, -1])
-absent_test=absent[900:, :, :].reshape([100, -1])
-present_test=present[900:, :, :].reshape([100, -1])
+absent_train=absent[:1500, :, :].reshape([1500, -1])
+present_train=present[:1500, :, :].reshape([1500, -1])
+train_data=np.concatenate((absent_train, present_train), axis=0)
+absent_test=absent[1500:, :, :].reshape([3300, -1])
+present_test=present[1500:, :, :].reshape([3300, -1])
 
 # normalize data
-absent_train=normalize(np.transpose(absent_train))
-present_train=normalize(np.transpose(present_train))
+train_data=normalize(np.transpose(train_data))
 absent_test=normalize(np.transpose(absent_test))
 present_test=normalize(np.transpose(present_test))
 
@@ -85,34 +125,38 @@ present_test=normalize(np.transpose(present_test))
 rd=np.sign(np.random.randn(measurements, ps[0]*ps[1])/10.0)
 
 # take compressed measurements with random matrix
-absent_train=np.matmul(rd, absent_train)
-present_train=np.matmul(rd, present_train)
+train_data=np.matmul(rd, train_data)
 absent_test=np.matmul(rd, absent_test)
 present_test=np.matmul(rd, present_test)
 
 
 with tf.Session() as sess:
   
-  # learn fruit dictionary
-  absent_dict, absent_alpha=LCA(absent_train, 200, 75, num_dict_features=k)
-  
-  
-
-  # learn car dictionary
-  present_dict, present_alpha=LCA(present_train, 200, 75, num_dict_features=k)
-  present_dict=np.matmul(np.transpose(rd), present_dict)
-  visualize_dict(present_dict, [20, 20], [36, 18])
+  # learn dictionary
+  dict_, alpha_=LCA(train_data, 300, 75, num_dict_features=k)
 
 
   ##############################test new images#################################
   
-  present_dict, present_alpha=LCA(present_test, 1, present_test.shape[1], D=absent_dict)
-  present_dict2, present_alpha2=LCA(present_test, 1, present_test.shape[1], D=present_dict)
+  present_testd, present_testa=LCA(present_test, 1, present_test.shape[1], D=dict_)
+  absent_testa, absent_testa=LCA(absent_test, 1, present_test.shape[1], D=dict_)
+  elm_data=np.concatenate((present_testa.transpose(), absent_testa.transpose()), axis=0)
+  elm_labels=np.concatenate((np.ones([3300, 1]), np.zeros([3300, 1])), axis=0)
 
+  r=np.random.random_integers(0, elm_data.shape[0])
+  elm_testd=elm_data[r[elm_data.shape[0]*0.9:], :]
+  elm_testl=elm_labels[r[elm_data.shape[0]*0.9:], :]
+  elm_data=elm_data[r[:elm_data.shape[0]*0.9], :]
+  elm_labels=elm_labels[r[:elm_data.shape[0]*0.9], :]
+
+  elm=ELM(elm_data, elm_labels, elm_testd, elm_testl, 65)
+
+  sess.run(tf.global_variables_initializer())
   
-
-
-
+  
+  for i in range(5000):
+    elm.optimize()
+    print(elm.accuracy)
 
 
 
