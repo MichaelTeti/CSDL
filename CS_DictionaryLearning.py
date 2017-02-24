600d3aa9e7aef38d0ac2cc22a1c60d2477595e79
michael teti <mtetifau@gmail.com>
2017-02-23 19:39:52 -0500

    working now

--- CS_DictionaryLearning.py
+++ CS_DictionaryLearning.py
@@ -5,6 +5,7 @@ import os
 import glob
 from skimage.util import view_as_windows
 from scipy.io import loadmat
+import functools
 
 
 def read_ims(directory, imsz):
@@ -38,10 +39,10 @@ def LCA(y, iters, batch_sz, num_dict_features=None, D=None):
            D: The dictionary to be used in the network. 
   '''
   assert(num_dict_features is None or D is None), 'provide D or num_dict_features, not both'
-  if num_dict_features is not None and D is None:
+  if num_dict_features is not None:
     D=np.random.randn(y.shape[0], num_dict_features)
   for i in range(iters):
-    batch=y[:, np.uint8(np.floor(np.random.rand(batch_sz)*y.shape[1]))]
+    batch=y[:, np.int32(np.floor(np.random.rand(batch_sz)*y.shape[1]))]
     D=tf.matmul(D, tf.diag(1/(tf.sqrt(tf.reduce_sum(D**2, 0))+1e-6)))
     a=tf.matmul(tf.transpose(D), batch)
     a=tf.matmul(a, tf.diag(1/(tf.sqrt(tf.reduce_sum(a**2, 0))+1e-6)))
@@ -60,6 +61,42 @@ def visualize_dict(D, d_shape, patch_shape):
   imshow(vis_d)
 
 
+def doublewrap(function):
+  """
+  A decorator decorator, allowing to use the decorator to be used without 
+  parentheses if not arguments are provided. All arguments must be optional.
+  """
+  @functools.wraps(function)
+  def decorator(*args, **kwargs):
+    if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+      return function(args[0])
+    else:
+      return lambda wrapee: function(wrapee, *args, **kwargs)
+  return decorator
+
+
+@doublewrap
+def define_scope(function, scope=None, *args, **kwargs):
+  """
+  A decorator for functions that define TensorFlow operations. The wrapped
+  function will only be executed once. Subsequent calls to it will directly
+  return the result so that operations are added to the graph only once.
+  The operations added by the function live within a tf.variable_scope(). If
+  this decorator is used with arguments, they will be forwarded to the
+  variable scope. The scope name defaults to the name of the wrapped
+  function.
+  """
+  attribute = '_cache_' + function.__name__
+  name = scope or function.__name__
+  @property
+  @functools.wraps(function)
+  def decorator(self):
+    if not hasattr(self, attribute):
+      with tf.variable_scope(name, *args, **kwargs):
+        setattr(self, attribute, function(self))
+    return getattr(self, attribute)
+  return decorator
+
 
 class ELM(object):
   '''Single, fully-connected hidden layer extreme learning machine.
@@ -68,35 +105,31 @@ class ELM(object):
           input_data: training examples with shape n x num_features.
           input_labels: n-dimensional one-hot vector. '''
   
-  def __init__(self, training_data, training_labels, testing_data, testing_labels, batch_sz):
-    self.X=training_data
-    self.Y=training_labels
-    self.x_=testing_data
-    self.y_=testing_labels
+  def __init__(self, data, labels):
+    
+    self.X=data
+    self.Y=labels
     self.batch_sz=batch_sz
     self.prediction
     self.optimize
     self.accuracy
 
+  @define_scope(initializer=tf.contrib.slim.xavier_initializer())
   def prediction(self):
-    batch=self.X[np.uint8(np.floor(np.random.rand(self.batch_sz)*self.X.shape[0])), :]
-    net=tf.matmul(batch, tf.random_normal([36*18, 36*18/2], dtype=tf.float32))
+    net=tf.matmul(self.X, tf.random_normal([400, 200], dtype=tf.float32))
     net=tf.nn.relu(net+tf.Variable(tf.constant(0.1)))
-    net=tf.matmul(net, tf.Variable(tf.truncated_normal([36*18/2, 1], dtype=tf.float32)))
-    net=tf.nn.sigmoid(net+tf.Variable(tf.constant(0.1, shape=[1, ])))
-    return sess.run(net)
+    net=tf.matmul(net, tf.Variable(tf.truncated_normal([200, 1], dtype=tf.float32)))
+    return tf.nn.sigmoid(net+tf.Variable(tf.constant(0.1, shape=[1, ])))
 
+  @define_scope
   def optimize(self):
-    cost=tf.reduce_sum((self.Y-self.prediction())**2, reduction_indices=0)
-    train_step=tf.train.AdamOptimizer(1e-4).minimize(cost)
-    return sess.run(train_step)   
+    cost=tf.reduce_sum((self.Y-self.prediction)**2, reduction_indices=0)
+    return tf.train.AdamOptimizer(1e-4).minimize(cost)  
  
+  @define_scope
   def accuracy(self):
-    accuracy=tf.reduce_mean(tf.cast(tf.equal(tf.round(self.prediction()), self.Y), tf.float32))
-    return sess.run(accuracy)
+   return tf.reduce_mean(tf.cast(tf.equal(tf.round(self.prediction), self.Y), tf.float32))
 
-  def validate(self):
-    validation_acc=
 
 
 
@@ -133,34 +166,41 @@ present_test=np.matmul(rd, present_test)
 with tf.Session() as sess:
   
   # learn dictionary
+  print('Learning features of the data...')
   dict_, alpha_=LCA(train_data, 300, 75, num_dict_features=k)
 
 
-  ##############################test new images#################################
+##############################test new images#######################################
+
+  batch_sz=200
   
   present_testd, present_testa=LCA(present_test, 1, present_test.shape[1], D=dict_)
   absent_testa, absent_testa=LCA(absent_test, 1, present_test.shape[1], D=dict_)
   elm_data=np.concatenate((present_testa.transpose(), absent_testa.transpose()), axis=0)
-  elm_labels=np.concatenate((np.ones([3300, 1]), np.zeros([3300, 1])), axis=0)
+  elm_labels=np.concatenate((np.ones([3300, ]), np.zeros([3300, ])), axis=0)
 
-  r=np.random.random_integers(0, elm_data.shape[0])
-  elm_testd=elm_data[r[elm_data.shape[0]*0.9:], :]
-  elm_testl=elm_labels[r[elm_data.shape[0]*0.9:], :]
-  elm_data=elm_data[r[:elm_data.shape[0]*0.9], :]
-  elm_labels=elm_labels[r[:elm_data.shape[0]*0.9], :]
+  r=np.int32(np.floor(np.random.rand(6600)*6600))
+  elm_testd=elm_data[r[6300:], :]
+  elm_testl=elm_labels[r[6300:]]
+  elm_data=elm_data[r[:6300], :]
+  elm_labels=elm_labels[r[:6300]]
 
-  elm=ELM(elm_data, elm_labels, elm_testd, elm_testl, 65)
+  x=tf.placeholder(dtype=tf.float32, shape=[None, k])
+  y=tf.placeholder(dtype=tf.float32, shape=[None, ])
 
+  elm=ELM(x, y)
+  
   sess.run(tf.global_variables_initializer())
-  
-  
-  for i in range(5000):
-    elm.optimize()
-    print(elm.accuracy)
+  a=0
+  for i in range(100000):
+    r=np.int32(np.floor(np.random.rand(batch_sz)*elm_data.shape[1]))
+    sess.run(elm.optimize, {x: elm_data[r, :], y: elm_labels[r]})
+    acc=sess.run(elm.accuracy, {x:elm_data[r, :], y: elm_labels[r]})
+    print('Iteration: %d   acc: %f\r'%(i, acc))
+    if i%100 == 0:
+      print('val_acc: %f'%(sess.run(elm.accuracy, {x: elm_testd, y: elm_testl})))
 
 
 
 
-  
-  
 
